32467bf9bc89bcb9bceca1dd5e9ff82dc500f236
Luiz Carlos Costa Gandra Filho <lcosta-g@c1r7p4.42sp.org.br>
2024-10-14 10:28:42 -0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,46 +6,47 @@
 /*   By: lcosta-g <lcosta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:09:35 by lcosta-g          #+#    #+#             */
-/*   Updated: 2024/10/11 13:36:06 by lcosta-g         ###   ########.fr       */
+/*   Updated: 2024/10/14 10:28:24 by lcosta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// TODO checar a da piscina e fazer melhorias
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	int		src_len;
 
-	i = 0;
-	src_len = ft_strlen(src);
-	// TODO Check src_len e size values
-	while (src[i] && i < size - 1)
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		i = 0;
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	return (src_len);
+	return (ft_strlen(src));
 }
-// TODO CRIAR TESTES COM A STRING VAZIA:
+
 /*
 #include <stdio.h>
 int	main(void)
 {
-	// TODO estudar variable-sized object:
-	// char	dst[size] = "--------------------";
-	// ----------------------------------------
 	char	dst[20] = "--------------------";
 	printf("ft_strlcpy(\"--------------------\", \"strlcpy example\", 20)\n");
 	printf("	retorna %zu\n", ft_strlcpy(dst, "strlcpy example", 20));
-	printf("	dst = %s\n", dst);
-	printf("\n");
+	printf("	dst = \"%s\"\n", dst);
+	printf("\n\n");
 	char	dst_2[8] = "--------";
 	printf("ft_strlcpy(\"--------\", \"testing strlcpy again\", 8)\n");
 	printf("	retorna %zu\n", ft_strlcpy(dst_2, "testing strlcpy again", 8));
-	printf("	dst = %s\n", dst_2);
+	printf("	dst = \"%s\"\n", dst_2);
+	printf("\n\n");
+	char	dst_3[] = "";
+	printf("ft_strlcpy(\"\", \"empty dest test\", 0)\n");
+	printf("	retorna %zu\n", ft_strlcpy(dst_3, "empty dest test", 0));
+	printf("	dst = \"%s\"\n", dst_3);
 	return (0);
 }
 */
