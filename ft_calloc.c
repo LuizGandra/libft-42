83886079ec2c7e1dac0004ba53cdcd1499f47f18
LuizGandra <luizc.gandra29@gmail.com>
2024-10-17 14:33:06 -0300

    Add main test for ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: lcosta-g <lcosta-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:16:17 by lcosta-g          #+#    #+#             */
-/*   Updated: 2024/10/16 18:44:15 by lcosta-g         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:31:10 by lcosta-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,17 @@ void	*ft_calloc(size_t nmemb, size_t size)
 }
 
 /*
+#include <stdio.h>
 int	main(void)
 {
-	// TODO write test
+	float	*arr;
+	int		i;
+
+	i = 0;
+	arr = (float *)ft_calloc(5, sizeof(float));
+	printf("Memória alocada e preenchida por zeros:\n");
+	while (i < 5)
+		printf("%f\n", arr[i++]);
 	return (0);
 }
 */
